42afc4fd7a6e46a4b6fadc259dd6daf1a9a27043
samueldebenedictis <samuel.debenedictis@yahoo.it>
2024-10-30 14:24:58 +0100

    chore: remove library first attempt working

--- src/components/_pages/Home.tsx
+++ src/components/_pages/Home.tsx
@@ -1,4 +1,6 @@
+'use client'
 import { Container, Flex, Heading } from '@chakra-ui/react';
+import {WorkingDemonstration} from 'components/Canvas';
 import FuseSearch from 'components/FuseSearch';
 import ThemeSwitcher from 'components/ThemeSwitcher';
 import React, { FC } from 'react';
@@ -9,6 +11,7 @@ const HomePage: FC<HomePageTypes> = ({ data }) => {
 
   return (
     <Container pt={4} maxW="container.lg">
+      <WorkingDemonstration width={400} height={200}/>
       <Flex justify={'space-between'}>
         <Heading pb={4}>samuel</Heading>
         <ThemeSwitcher />
