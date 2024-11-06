2e1edac78d2fb40d70e8b0da945b17875020c505
samueldebenedictis <samuel.debenedictis@yahoo.it>
2024-11-06 18:05:17 +0100

    chore: change icon

--- src/components/_pages/Home.tsx
+++ src/components/_pages/Home.tsx
@@ -2,7 +2,7 @@ import Search from 'components/Search';
 import React, { FC, useEffect } from 'react';
 import { IconBase } from 'react-icons';
 import { HomePageTypes } from 'types/Pages';
-import { FaRegMoon, FaRegSun } from "react-icons/fa"
+import { FaMoon, FaSun } from "react-icons/fa"
 
 const HomePage: FC<HomePageTypes> = ({ data }) => {
   const { sketches } = data;
@@ -25,13 +25,12 @@ const HomePage: FC<HomePageTypes> = ({ data }) => {
     console.log(document.body.classList)
   }
 
-
   return (
-    <div className="justify-self-center w-3/4">
-      <div className="p-4 text-5xl bg-sky-500 dark:bg-red-500 justify-between items-center flex">
+    <div className="justify-self-center sm:w-3/4 w-full p-4">
+      <div className="text-2xl font-bold bg-sky-500 dark:bg-red-500 justify-between items-center flex">
         <h1>samuel</h1>
         <button>
-          <FaRegMoon onClick={toggleTheme} size={20}/>
+          <FaMoon onClick={toggleTheme} size={20}/>
         </button>
       </div>
       <div>
