053421103e87c01581968cafa2326b4d2d383d19
samueldebenedictis <samuel.debenedictis@yahoo.it>
2024-11-06 18:49:44 +0100

    chore: refactor theme and types

--- src/components/_pages/Home.tsx
+++ src/components/_pages/Home.tsx
@@ -1,16 +1,22 @@
 import Search from 'components/Search';
-import React, { FC, useEffect } from 'react';
-import { IconBase } from 'react-icons';
-import { HomePageTypes } from 'types/Pages';
-import { FaMoon, FaSun } from "react-icons/fa"
+import React, { FC, useEffect, useState } from 'react';
+import { FaMoon, FaSun } from 'react-icons/fa';
 
-const HomePage: FC<HomePageTypes> = ({ data }) => {
+export interface HomePageProps {
+  data: {
+    sketches: string[];
+  };
+}
+
+
+const HomePage: FC<HomePageProps> = ({ data }) => {
   const { sketches } = data;
-
+  const [theme, setTheme] = useState('light');
   useEffect(() => {
     const theme = localStorage.getItem('theme');
     if (theme === 'dark') {
       document.body.classList.add('dark');
+      setTheme('dark');
     }
   }, []);
 
@@ -18,20 +24,27 @@ const HomePage: FC<HomePageTypes> = ({ data }) => {
     if (document.body.classList.contains('dark')) {
       document.body.classList.remove('dark');
       localStorage.setItem('theme', 'light');
+      setTheme('light');
     } else {
       document.body.classList.add('dark');
       localStorage.setItem('theme', 'dark');
+      setTheme('dark');
     }
-    console.log(document.body.classList)
-  }
+  };
 
   return (
     <div className="justify-self-center sm:w-3/4 w-full p-4">
-      <div className="text-2xl font-bold bg-sky-500 dark:bg-red-500 justify-between items-center flex">
+      <div className="text-2xl font-bold justify-between items-center flex">
         <h1>samuel</h1>
-        <button>
-          <FaMoon onClick={toggleTheme} size={20}/>
-        </button>
+        {theme === 'dark' ? (
+          <button>
+            <FaSun onClick={toggleTheme} size={20} />
+          </button>
+        ) : (
+          <button>
+            <FaMoon onClick={toggleTheme} size={20} />
+          </button>
+        )}
       </div>
       <div>
         <Search sketches={sketches} />
--- src/types/Pages.ts
+++
@@ -1,5 +0,0 @@
-export interface HomePageTypes {
-  data: {
-    sketches: string[];
-  };
-}
