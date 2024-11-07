97ca633ae46e37c9a00fa43c60c5c81a116e24b4
samueldebenedictis <samuel.debenedictis@yahoo.it>
2024-11-07 12:10:36 +0100

    chore: prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,10 @@ module.exports = {
   darkMode: 'selector',
   theme: {
     extend: {
-        fontFamily: {
-            "poppins": ['Poppins', 'sans-serif']
-        }
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
     },
-},
+  },
   plugins: [],
 };
